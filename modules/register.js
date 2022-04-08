3838c903f45868154b4ac4f990c89eb31b98e8df
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-04-07 23:50:49 -0300

    update 0.2b

--- modules/register.js
+++ modules/register.js
@@ -9,7 +9,7 @@ async function register (req, res)
     bcrypt.hash(password, 12, async (err, hash) =>
     {
         console.log(hash.length);
-        const resp = await database.query(`INSERT INTO public.user (username, password, email, name, privilege, created_at, created_by) VALUES ($1, $2, $3, $4, 1, to_timestamp(${Date.now()} / 1000.0), -1);`, [username, password, email, name])
+        const resp = await database.query(`INSERT INTO public.user (username, password, email, name, privilege, created_at, created_by) VALUES ($1, $2, $3, $4, 1, to_timestamp(${Date.now()} / 1000.0), -1);`, [username, hash, email, name])
         .catch(error => {
             console.log(error);//.detail
         }); //req.query["username"], hash
